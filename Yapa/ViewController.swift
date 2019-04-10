1936a407ae42036837df8a69841e41d00338ae26
Daniel Moreh <dmoreh@gmail.com>
2019-04-10 09:43:27 -0700

    AVAudioPlayer test

--- Yapa/ViewController.swift
+++ Yapa/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        self.playLocalFile()
+
     }
 
+    func playUsingAVAudioPlayer(url: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print(error)
+        }
+    }
+
+    func playLocalFile() {
+        guard let filePath = Bundle.main.path(forResource: "freaks", ofType: "mp3") else {
+            print("File does not exist in the bundle.")
+            return
+        }
+
+        let url = URL(fileURLWithPath: filePath)
+
+        playUsingAVAudioPlayer(url: url)
+    }
 
 }
 
