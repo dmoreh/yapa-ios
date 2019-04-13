1663401c7505b6aaa57b6e081726196b62199e9f
Daniel Moreh <daniel@woebot.io>
2019-04-13 11:51:26 -0700

    Custom colors

---
+++ Yapa/Utils/UIColor+Yapa.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Yapa.swift
+//  Yapa
+//
+//  Created by Daniel Moreh on 4/13/19.
+//  Copyright © 2019 Daniel Moreh. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var lightOrange: UIColor {
+        return UIColor.init(red: 244.0 / 255.0, green: 198.0 / 255.0, blue: 152.0 / 255.0, alpha: 1)
+    }
+
+    static var orange: UIColor {
+        return UIColor.init(red: 234.0 / 255.0, green: 110.0 / 255.0, blue: 55.0 / 255.0, alpha: 1)
+    }
+}
